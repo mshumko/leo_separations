c2344f97bb1bd7abc34c4847a8d35e6c01d8ffc5
mshumko <msshumko@gmail.com>
2018-04-11 09:55:03 -0600

    Made the summary plots. It appears to work, and handle the sign issue with the AC6 separation.

--- lap.py
+++ lap.py
@@ -3,7 +3,7 @@
 
 import csv
 import matplotlib.pyplot as plt
-from matplotlib.dates import num2date
+import matplotlib.dates
 import dateutil.parser
 import numpy as np
 from datetime import datetime, timedelta
@@ -42,10 +42,28 @@ class Lap():
         self._load_ac_data(tRange, acDtype)
         self._match_lat_lag(tRange)
 
-        fig, ax = plt.subplots(3)
-        self._plot_fb(tRange, ax[0])
+        fig, ax = plt.subplots(3, figsize=(8, 9))
+        self._plot_fb(tRange, ax[0], axPos=ax[2])
+        self._plot_ac(tRange, ax[1], axPos=ax[2])
 
+        ### Plot Adjustments ###
+        titleStr = ('FU{} - AC6{} Lapping event | {} | {} s lag ({} km)').format(
+                                    self.fb_id, self.ac_id, tRange[0].date(), 
+                                    round(np.abs(self.ac_time_lag), 1), 
+                                    round(np.abs(self.ac_time_lag)*7.5, 1))
+        ax[0].set_title(titleStr)
+        ax[-1].set_xlabel('UTC')
 
+        # Set xlims for all subplots
+        ax[0].set_xlim([timedelta(seconds=self.fb_time_shift) + t for t in tRange])
+        ax[1].set_xlim([timedelta(seconds=self.ac_time_lag+self.fb_time_shift) + t for t in tRange])
+        ax[2].set_xlim([timedelta(seconds=self.fb_time_shift) + t for t in tRange])
+        ax[2].legend()
+        # for a in ax[1:]:
+        #     a.set_xlim(tRange)
+        for a in ax: # Format time stamps for all subplots
+            myFmt = matplotlib.dates.DateFormatter('%H:%M:%S')
+            a.xaxis.set_major_formatter(myFmt)
         return
 
     def _load_fb_data(self, tRange):
@@ -76,6 +94,7 @@ class Lap():
         # Find the first order correction (unsigned)
         sepInd = np.where(self.sep['dateTime'] > tRange[0])[0][0]
         self.ac_time_lag = self.sep['d'][sepInd]/7.5
+        #print('Zeroth order time lag', self.ac_time_lag)
         # Determine the sign of self.ac_time_lag
         dLatPast = np.where(self.acData['dateTime'] > 
                     tRange[0] - timedelta(seconds=self.ac_time_lag))[0][0]
@@ -93,9 +112,9 @@ class Lap():
         # Now find the higher order correction (signed)
         self.ac_time_lag = ( self.hr['Time'][fbSind] - 
             self.acData['dateTime'][acInd] ).total_seconds()
-        
-        print(self.hr['Lat'][fbSind], self.acData['lat'][dLatPast], 
-                    self.acData['lat'][dLatFuture])
+        #print('First order time lag', self.ac_time_lag)
+        #print(self.hr['Lat'][fbSind], self.acData['lat'][dLatPast], 
+        #            self.acData['lat'][dLatFuture])
 
         return
 
@@ -112,17 +131,58 @@ class Lap():
         self.sep['d'] = np.array([float(f) for f in rData[:, 1]])
         return
 
-    def _plot_fb(self, tRange, ax):
+    def _plot_fb(self, tRange, axCounts, axPos=None, axL=True):
         """ This method plots the FIREBIRD col counts data. """
         normTind = np.where((self.hr['Time'] > tRange[0]) & 
                             (self.hr['Time'] < tRange[1]))[0]
         shiftInd = np.where(
             (self.hr['Time'] > tRange[0]-timedelta(seconds=self.fb_time_shift)) & 
             (self.hr['Time'] < tRange[1]-timedelta(seconds=self.fb_time_shift)))[0]
+        fbTimes = np.array([t+timedelta(seconds=self.fb_time_shift) 
+                            for t in self.hr['Time'][normTind]])
         for E, Elabel in enumerate(self.hr['Col_counts'].attrs['ELEMENT_LABELS']):
-            ax.plot(self.hr['Time'][normTind], self.hr['Col_counts'][shiftInd, E],
+            axCounts.plot(fbTimes, self.hr['Col_counts'][normTind, E],
                     label=Elabel)
-        ax.set(ylabel='FU{} counts/bin'.format(self.fb_id), yscale='log')
+        axCounts.set(ylabel='FU{} counts/bin'.format(self.fb_id), yscale='log')
+        axCounts.legend()
+
+        if axPos is not None:
+            axPos.plot(self.hr['Time'][normTind], self.hr['Lon'][normTind], 
+                        label='FU{} Lon'.format(self.fb_id))
+            axPos.set_ylabel('Longitude [deg]')
+        if axL:
+            axL = axCounts.twinx()
+            axL.plot(self.hr['Time'][normTind], self.hr['McIlwainL'][normTind], 'k')
+            axL.set_ylabel('McIlwain L (T89) (black curve)')
+            axL.set_ylim(3, 10)
+        return
+
+    def _plot_ac(self, tRange, axCounts, axPos=None, axL=True):
+        """
+        This method plots the AC6 dosimiter count rates and position 
+        in a similar way to _plot_fb()
+        """
+        # Plot dosimiter counts
+        for key in ['dos1rate', 'dos2rate', 'dos3rate']:
+            validCounts = np.where((self.acData[key] != -1E31) & 
+                            (self.acData['dateTime'] > tRange[0]+timedelta(seconds=self.ac_time_lag)) & 
+                            (self.acData['dateTime'] < tRange[1]+timedelta(seconds=self.ac_time_lag)) )[0]
+            axCounts.plot(self.acData['dateTime'][validCounts], 
+                        self.acData[key][validCounts],
+                        label=key)
+        # Plot position
+        if axPos is not None:
+            shiftedTimes = np.array([timedelta(seconds=-self.ac_time_lag) + t
+                            for t in self.acData['dateTime']])
+            axPos.plot(shiftedTimes[validCounts], self.acData['lon'][validCounts], 
+                        label='AC6-{} lon'.format(self.ac_id))
+        if axL:
+            axL = axCounts.twinx()
+            validL = np.where(self.acData['Lm_OPQ'] != -1E31)[0]
+            axL.plot(self.acData['dateTime'][validL], self.acData['Lm_OPQ'][validL], 'k')
+            axL.set_ylabel('McIlwain L (OPQ) (black curve)')
+            axL.set_ylim(3, 10)
+        
         return
 
 
@@ -145,5 +205,9 @@ if __name__ == '__main__':
     ac_id = 'A'
     dPath = './data/2018-02-26_2018-03-29_FU{}_AC6{}_dist.csv'.format(fb_id, ac_id)
     lapTime = '0326T0213'
+
     l = Lap(dPath, fb_id, ac_id)
-    l.plot_lap_event(lapDict[lapTime]['tRange'])
+    l.plot_lap_event(lapDict[lapTime]['tRange'], acDtype='survey')
+
+    plt.tight_layout()
+    plt.show()
